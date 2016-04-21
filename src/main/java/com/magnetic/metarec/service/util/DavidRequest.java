6451f3c4e285a5dbf918fba362568c620a961aee
David Whitesell <david.whitesell@magnetic.com>
2016-04-21 12:35:03 -0400

    Put orphaned lines into DavidRandomUtil class:

---
+++ src/main/java/com/magnetic/metarec/service/util/DavidRequest.java
@@ -0,0 +1,69 @@
+package com.magnetic.metarec;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Created by dwhitesell on 4/20/16.
+ */
+public class DavidRequest implements Runnable{
+
+
+    private WebRecRequestSchedulingController controller;
+    private URI requestURI;
+    private HttpGet httpConnect;
+    private String request_id;
+
+    public DavidRequest(URI uri, WebRecRequestSchedulingController control, String req_id)
+    {
+        requestURI = uri;
+        controller = control;
+        httpConnect = new HttpGet(requestURI);
+        request_id = req_id;
+    }
+
+    public void run()
+    {
+        try {
+            String responseFromRequest = getResponseFromRequest();
+        } catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    public String getResponseFromRequest() throws Exception
+    {
+        String response = "";
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+            @Override
+            public String handleResponse(
+                final HttpResponse response) throws ClientProtocolException, IOException {
+                int status = response.getStatusLine().getStatusCode();
+                if (status >= 200 && status < 300) {
+                    HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity) : null;
+                } else {
+                    throw new ClientProtocolException("Unexpected response status: " + status);
+                }
+            }
+
+        };
+        response = httpclient.execute(httpConnect, responseHandler);
+        System.out.println("----------------------------------------");
+        System.out.println(response);
+        return response;
+    }
+}

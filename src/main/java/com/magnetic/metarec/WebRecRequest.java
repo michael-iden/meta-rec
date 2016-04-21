721a711bdb1658f0195f0b149d26a2593d211ead
David Whitesell <david.whitesell@magnetic.com>
2016-04-21 11:41:39 -0400

    Added some functionality for the request building in the controller class, and preparing to run the request and parse the result as part of the request class itself

--- src/main/java/com/magnetic/metarec/WebRecRequest.java
+++ src/main/java/com/magnetic/metarec/WebRecRequest.java
@@ -1,54 +1,38 @@
 package com.magnetic.metarec;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+
+import java.net.URI;
+
 /**
  * Created by dwhitesell on 4/20/16.
  */
-public class WebRecRequest {
+public class WebRecRequest implements Runnable{
 
-    private String client;
-    private String sessionId;
-    private String webreqZones;
 
-    private String webrecServerAddress;
+    private WebRecRequestSchedulingController controller;
+    private URI requestURI;
+    private HttpGet httpConnect;
 
-    private String pageType;
+    public WebRecRequest(URI uri, WebRecRequestSchedulingController control)
+    {
+        requestURI = uri;
+        controller = control;
+        httpConnect = new HttpGet(requestURI);
 
-    private String channel;         // for 'chn' parameter in wr.do GET request
-    private String productId;      // For 'cpc' parameter in wr.do GET request
+    }
 
-    private String categoryId;     //  'ckc' parameter in wr.do GET request
-    private String brandName;      // 'bnm' parameter in wr.do GET request
-    private String productInPage;     // corresponds to the 'pips' param for wr.do GET request
-    private String products;        // corresponds to the 'prods' param for wr.do GET request
-    private String inventoryCategory;   // corresponds to 'scckc' param for wr.do GET request
-    private String inventoryAttributeName;      // corresponds to 'scattr' param for wr.do GET request
-    private String inventoryAttributeValue;     // corresponds to 'scval' param for wr.do GET request
-    private String filteringAttribute1;
-    private String filteringAttribute2;
-    private String filteringAttribute1Val;  // corresponds to 'mbfa_filtering_attribute_1' param for wr.do GET request
-    private String filteringAttribute2Val;  // corresponds to 'mbfa_filtering_attribute_2' param for wr.do GET request
-    private String orderId;     // corresponds to 'order' param for wr.do GET request
-    private String overrideConsumerId;  // corresponds to 'mbcc' parameter for wr.do GET request
-    private String overrideEmail;       // corresponds to 'email' param of wr.do GET request
-    private String outOfStock;      // corresponds to 'ostock' param of wr.do GET request
-    private String keywords;    // corresponds to 'kws' param of wr.do GET request
-    private String language;    // corresponds to 'lang' param for wr.do GET request
-
-    public WebRecRequest(String clientName,
-                         String serverEnvironment,
-                         String pageTypeParam,
-                         String zoneIds)
+    public void run()
     {
 
     }
 
-    public WebRecRequest()
+    public String getResponseFromRequest()
     {
-
+        String response = "";
+        return response;
     }
 
-    public WebRecRequest(String clientNm, String session)
-    {
-
-    }
 }

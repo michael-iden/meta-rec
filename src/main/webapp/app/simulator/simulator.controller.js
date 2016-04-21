ac94a0e976ebc81aa419d3d49f2f61c4ac78d5b2
michael.iden <michael.iden@magnetic.com>
2016-04-20 21:17:23 -0400

    Added a client dropdown to the simulator page

--- src/main/webapp/app/simulator/simulator.controller.js
+++ src/main/webapp/app/simulator/simulator.controller.js
@@ -5,34 +5,18 @@
         .module('metaRecApp')
         .controller('SimulatorController', SimulatorController);
 
-    SimulatorController.$inject = ['$scope', 'Principal', 'LoginService', '$state', '$http'];
+    SimulatorController.$inject = ['$scope', '$state', '$http'];
 
-    function SimulatorController ($scope, Principal, LoginService, $state, $http) {
-        var vm = this;
+    function SimulatorController ($scope, $state, $http) {
 
-        vm.account = null;
-        vm.isAuthenticated = null;
-        vm.login = LoginService.open;
-        vm.register = register;
-        $scope.$on('authenticationSuccess', function() {
-            getAccount();
-        });
+        $scope.clients = [
+            'JOANN',
+            'WESTMARINE',
+            'SKECHERS',
+            'JOURNEYS',
+            'TEST'
+        ];
 
-        getAccount();
-
-        function getAccount() {
-            Principal.identity().then(function(account) {
-                vm.account = account;
-                vm.isAuthenticated = Principal.isAuthenticated;
-            });
-        }
-        function register () {
-            $state.go('register');
-        }
-
-        $http.get('/test').success(function (data) {
-            console.log(data);
-        });
-
+        $scope.selectedClient = { value: $scope.clients[0] };
     }
 })();
